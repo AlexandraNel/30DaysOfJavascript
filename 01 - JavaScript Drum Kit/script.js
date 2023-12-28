d251eb76d9225de8a5170d4e5865f6d9c7849651
Lex <info@alexandranel.com>
2023-12-28 21:22:49 +1100

    have done day 02, javascript clock

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,5 +1,28 @@
-addEventListener("keydown", function(event){
-console.log(event.keyCode);
 
- var audio = document.querySelector("audio[data-key=]")
-});
+
+
+
+
+function playSound(event){
+    var audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    var key= document.querySelector(`.key[data-key="${event.keyCode}"]`);
+   
+    if (!audio) return; //stops function if no audio 
+    audio.currentTime = 0; //reqind to start
+    audio.play();
+   
+    key.classList.add("playing");
+   };
+
+
+
+function removeTransition(event){
+    if (event.propertyName !== "transform" ) return; 
+    this.classList.remove("playing"); 
+ };
+
+var keys = document.querySelectorAll(".key");
+
+keys.forEach( key => key.addEventListener("transitionend", removeTransition));
+addEventListener("keydown", playSound);
+
---
+++ 02 - JS and CSS Clock/javascript.js
@@ -0,0 +1,31 @@
+
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+const allHands = document.querySelectorAll(".hand");
+
+function setDate() {
+    const now = new Date()
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+    const minutes = now.getMinutes();
+    const minuteDegrees = ((minutes / 60) * 360) + 90;
+
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 12) * 360) + 90;
+
+    if (seconds === 0 || minutes === 0 || hours === 0) {
+        allHands.forEach(hand => hand.style.transition = "none");
+    } else {
+        allHands.forEach(hand => hand.style.transition = "");
+    }
+
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
