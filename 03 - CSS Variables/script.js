4f720f072b52617f309e0a9728c273e90449e855
Lex <info@alexandranel.com>
2023-12-29 13:34:09 +1100

    day 3

---
+++ 03 - CSS Variables/script.js
@@ -0,0 +1,12 @@
+
+
+var inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate(){
+  var suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  console.log(suffix);  
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
